59983cf2a2bd64031a3834d8d69db684a03f33a3
Cade Medearis <108916110+cadeakins@users.noreply.github.com>
2025-07-05 06:04:56 +0000

    Finished assignment.

--- script.js
+++ script.js
@@ -6,4 +6,14 @@ const punchline = document.getElementById('punchline');
 // Add an event listener to the button
 jokeBtn.addEventListener('click', function() {
   console.log('Getting a joke...');
+
+
+//Fetch joke from official joke api
+fetch('https://official-joke-api.appspot.com/jokes/programming/random')
+  .then(response => response.json()) // Parse the JSON response
+  .then(data => {
+      console.log(data); // Log the data to the console
+      setup.textContent = data[0].setup; // Set the setup text
+      punchline.textContent = data[0].punchline; // Set the punchline text
+    });
 });
